0ee8a889b8fa7022e21a4f29441f0abf43d12d35
bmmlms <bmmlms@users.noreply.github.com>
2011-12-14 21:54:35 +0000

    git-svn-id: svn://mistake.ws/streamwriter@314 c9e4e0f1-c7f3-4966-b35a-9197a887d05d

--- streamwriter/streamwriter.dpr
+++ streamwriter/streamwriter.dpr
@@ -1,7 +1,7 @@
 {
     ------------------------------------------------------------------------
     streamWriter
-    Copyright (c) 2010 Alexander Nottelmann
+    Copyright (c) 2010 Alexander Nottelmann TEST
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
