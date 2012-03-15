f20e00fe2c6635756c590de3891571944e41884e
bmmlms <bmmlms@users.noreply.github.com>
2012-03-15 23:13:29 +0000

    git-svn-id: svn://mistake.ws/streamwriter@380 c9e4e0f1-c7f3-4966-b35a-9197a887d05d

--- streamwriter/AppData.pas
+++ streamwriter/AppData.pas
@@ -474,7 +474,7 @@ begin
   FRecoveryFile := FStorage.DataDir + 'streamwriter_data_recovery.dat';
 
   // The number of the current build
-  FBuildNumber := 379;
+  FBuildNumber := 380;
 
   // This builds a large string used to generate the about-window
   BuildThanksText;
