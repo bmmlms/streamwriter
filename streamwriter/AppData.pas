4c416c857c1911f5d0928b9b61e7352ca66a0208
bmmlms <bmmlms@users.noreply.github.com>
2012-12-28 23:53:30 +0000

    - This is the real version 4.3 ;-)
    git-svn-id: svn://mistake.ws/streamwriter@444 c9e4e0f1-c7f3-4966-b35a-9197a887d05d

--- streamwriter/AppData.pas
+++ streamwriter/AppData.pas
@@ -481,7 +481,7 @@ begin
   FRecoveryFile := FStorage.DataDir + 'streamwriter_data_recovery.dat';
 
   // The number of the current build
-  FBuildNumber := 442;
+  FBuildNumber := 444;
 
   // This builds a large string used to generate the about-window
   BuildThanksText;
