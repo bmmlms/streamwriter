f545cf7167ce3a656246e649640db4d444785c7d
bmmlms <bmmlms@users.noreply.github.com>
2018-07-01 16:26:15 +0000

    - Updated copyright information
    git-svn-id: svn://mistake.ws/streamwriter@762 c9e4e0f1-c7f3-4966-b35a-9197a887d05d

--- streamwriter/controls/CutView.pas
+++ streamwriter/controls/CutView.pas
@@ -1,7 +1,7 @@
 ﻿{
     ------------------------------------------------------------------------
     streamWriter
-    Copyright (c) 2010-2017 Alexander Nottelmann
+    Copyright (c) 2010-2018 Alexander Nottelmann
 
     Portions created by Ralf Kruse
 
