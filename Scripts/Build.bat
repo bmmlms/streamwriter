5facf12e8487b2c113690232b72cf9ed4e0e3ccc
bmmlms <bmmlms@users.noreply.github.com>
2024-02-01 19:45:22 +0100

    Update build script

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -32,24 +32,30 @@ goto end
 :build
   cd "%SOURCEDIR%"
 
-  instantfpc "%SCRIPTSDIR%\SetGitVersion.pas" streamwriter.lpi streamwriter-%~1.lpi
+  instantfpc "%SCRIPTSDIR%\SetGitVersion.pas" "streamwriter.lpi" "streamwriter-%~1.lpi"
   if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
-  powershell -Command "(gc streamwriter-%~1.lpi) -replace 'x86_64', '%~1' | Out-File streamwriter-%~1.lpi"
+  powershell -Command "(gc 'streamwriter-%~1.lpi') -replace '-x86_64.dll', '-%~1.dll' | Out-File 'streamwriter-%~1.lpi'"
   if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
   REM Build executables
-  lazbuild --build-all --cpu=%~1 --build-mode=Release --quiet --quiet streamwriter-%~1.lpi
+  lazbuild --build-all --cpu=%~1 --build-mode=Release --quiet --quiet "streamwriter-%~1.lpi"
   if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
-  del streamwriter-%~1.lpi
+  del "streamwriter-%~1.lpi"
   if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
   REM Build addons
   for /R "..\Addons" %%f in (*.lpi) do (
     cd "%%~dpf"
 
-    lazbuild --build-all --cpu=%~1 --build-mode=Release --quiet --quiet "%%~nxf"
+    powershell -Command "(gc '%%~nxf') -replace '-x86_64.dll', '-%~1.dll' | Out-File '%%~nf-%~1.lpi'"
+    if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
+
+    lazbuild --build-all --cpu=%~1 --build-mode=Release --quiet --quiet "%%~nf-%~1.lpi"
+    if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
+
+    del "%%~nf-%~1.lpi"
     if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
   )
 
@@ -76,26 +82,26 @@ goto end
 :setup
   cd "%PROJECTDIR%\Setup"
 
-  powershell -Command "(gc %APPNAME%.iss) -replace 'x86_64', '%~1' | Out-File -encoding ASCII %APPNAME%-%~1.iss"
+  powershell -Command "(gc '%APPNAME%.iss') -replace 'AppCpu \"x86_64\"', 'AppCpu \"%~1\"' | Out-File -encoding ASCII '%APPNAME%-%~1.iss'"
 
   if "%~1"=="i386" (
-    powershell -Command "(gc %APPNAME%-%~1.iss) -replace 'ArchitecturesAllowed=x64', '' | Out-File -encoding ASCII %APPNAME%-%~1.iss"
+    powershell -Command "(gc '%APPNAME%-%~1.iss') -replace 'ArchitecturesAllowed=x64', '' | Out-File -encoding ASCII '%APPNAME%-%~1.iss'"
     if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
 
-    powershell -Command "(gc %APPNAME%-%~1.iss) -replace 'ArchitecturesInstallIn64BitMode=x64', '' | Out-File -encoding ASCII %APPNAME%-%~1.iss"
+    powershell -Command "(gc '%APPNAME%-%~1.iss') -replace 'ArchitecturesInstallIn64BitMode=x64', '' | Out-File -encoding ASCII '%APPNAME%-%~1.iss'"
     if %ERRORLEVEL% GEQ 1 exit /b %ERRORLEVEL%
   )
 
-  "%INNO%" /O"%OUTDIR%\%~1" %APPNAME%-%~1.iss
+  "%INNO%" /O"%OUTDIR%\%~1" "%APPNAME%-%~1.iss"
   if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
-  del %APPNAME%-%~1.iss
+  del "%APPNAME%-%~1.iss"
   if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
-  REM type "%OUTDIR%\%~1\%APPNAME%_setup.exe" | "%PLINK%" -batch gaia osslsigncode-sign.sh > "%OUTDIR%\%~1\%APPNAME%_setup-signed.exe"
-  REM if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
-  REM move /y "%OUTDIR%\%~1\%APPNAME%_setup-signed.exe" "%OUTDIR%\%~1\%APPNAME%_setup.exe"
-  REM if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
+  type "%OUTDIR%\%~1\%APPNAME%_setup.exe" | "%PLINK%" -batch gaia osslsigncode-sign.sh > "%OUTDIR%\%~1\%APPNAME%_setup-signed.exe"
+  if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
+  move /y "%OUTDIR%\%~1\%APPNAME%_setup-signed.exe" "%OUTDIR%\%~1\%APPNAME%_setup.exe"
+  if %ERRORLEVEL% GEQ 1 exit /B %ERRORLEVEL%
 
   exit /b 0
 
