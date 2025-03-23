133d4b4b2d97b6ba7184ec759e735427af5f236e
bmmlms <bmmlms@users.noreply.github.com>
2025-03-23 03:33:46 +0100

    Update copyright

--- Source/Controls/CutView.pas
+++ Source/Controls/CutView.pas
@@ -1,7 +1,7 @@
 ﻿{
     ------------------------------------------------------------------------
     streamWriter
-    Copyright (c) 2010-2024 Alexander Nottelmann
+    Copyright (c) 2010-2025 Alexander Nottelmann
 
     Portions created by Ralf Kruse
 
